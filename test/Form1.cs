1111660a396c69ea809db89b77053f40053e6c10
anselotti <anssi.kosunen@gmail.com>
2022-09-30 10:15:59 +0200

    testing git

---
+++ test/Form1.cs
@@ -0,0 +1,16 @@
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var a = 0;
+
+        }
+    }
+}
